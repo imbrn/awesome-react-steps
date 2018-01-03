6c9dd84013f7656677faffcc4f8e03abce23ab25
Bruno C. Couto <brunodev02221@gmail.com>
2018-01-03 15:46:16 -0200

    Add support to PostCSS

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,7 +5,16 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
+          },
+          { loader: "postcss-loader" }
+        ],
         include: path.resolve(__dirname, "../")
       }
     ]
