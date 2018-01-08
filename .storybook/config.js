5275388ea40c6e9e0f4e20054012a610b714c7d4
Bruno C. Couto <brunodev02221@gmail.com>
2018-01-08 17:52:13 -0200

    Move storybook stories to .stories folder

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from "@storybook/react";
 
 function loadStories() {
-  require("../stories/arrows.js");
+  require("../.stories/arrows.js");
 }
 
 configure(loadStories, module);
